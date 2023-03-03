2a7da424eebbd04117a465c4344b7a153f2a9645
Sidney Basa <basa206@hotmail.com>
2023-03-02 23:27:21 -0800

    developed departments section in inquirer

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const database = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: 'Vinpocetine7#',
+        database: 'employee_database'
 
     },
     console.log(`Connected to the employee_database`)
@@ -34,23 +35,28 @@ function startApplication() {
                 choices: ['view all departments', 'view all roles', 'view all employees','add a department','add a role','add an employee','exit'],
             }
         ]).then((response) => {
-            console.log("Value of response from .then promise", response)
-            let newVariable = response;
-            let newVariable3 = '';
+            // console.log("Value of response from .then promise", response)
+            // let newVariable = response;
+            // let newVariable3 = '';
+            
+            
+            // console.log(response)
             // console.log("Value of newVariable", newVariable)
             // console.log("Value of typeof(newVariable)", typeof(newVariable))
-            // console.log("Stringify values of newVariable object", JSON.stringify(newVariable))
+            // console.log("((TEST))Stringify values of newVariable object", JSON.stringify(newVariable))
             // let newVariable2 = JSON.stringify(newVariable)
             // console.log("Value of newVariable2", newVariable2)
             // console.log("property of object newVariable:", JSON.stringify(newVariable,["start_menu"]))
             // console.log(JSON.stringify(newVariable))
             // console.log(newVariable, `${value}`)
-            for (const [key, value] of Object.entries(newVariable))
-            {
+
+            // New sytax from stack overflow:
+            // for (const [key, value] of Object.entries(newVariable))
+            // {
                 // console.log(`${value}`)
-                newVariable3 = value
+                // newVariable3 = value
                 // console.log("Value of newVariable3:", newVariable3) 
-            }
+            // }
             // console.log("Value of newVariable3", newVariable3)
             // console.log("property of object newVariable2:", JSON.stringify(newVariable2,["start_menu"]))
             // console.log("Value of newVariable2.value", newVariable2.value)
@@ -60,15 +66,18 @@ function startApplication() {
             // console.log("Value of response.value from .then promise", response.value)
             // console.log("Value of response[0] from .then promise", response[0])
             // console.log("Value of response from .then promise", response)
-            switch (newVariable3) {
+            switch (response.start_menu) {
                 case "view all departments":
                     // console.log("ADD all departments")
-                    viewAllDepartments();
-                    break;
+                    // viewAllDepartments();
+                    // return all view departments
+                    return viewAllDepartments()
+                    // break;
                     
                 case "exit":
                     quit();
                     break;
+
                 default:
                         break;
             }
@@ -77,11 +86,46 @@ function startApplication() {
 
 function viewAllDepartments() {
     console.log("Add function viewAllDepartments")
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select one of the Departments:',
+            name: 'department_list',
+            choices: ['Department 1', 'Department 2', 'Department 3', 'exit'],
+        }
+    ]).then((response) => {
+        switch (response.department_list) {
+            case "Department 1":
+                makemore();
+                break;
+                // console.log("ADD all departments")
+                // viewAllDepartments();
+                // return all view departments
+                
+            case "Department 2":
+                makemore();
+                break;
+            case "Department 3":
+                makemore();
+               break;
+            case "exit":
+                quit();
+                break;
+
+            default:
+                    break;
+        }
+    })
     
 }
 
 function quit() {
     console.log("Goodbye!");
+    process.exit(0);
+}
+
+function makemore() {
+    console.log("Need to develop this more Goodbye!");
     process.exit(0);
 }
 
